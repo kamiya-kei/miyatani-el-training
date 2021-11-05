1d103ec277842e074038812b8d250bacd9d0dd46
kamiya-kei <miyahei38@gmail.com>
2021-11-05 17:49:35 +0900

    taskのquery定義

---
+++ app/graphql/types/task_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class TaskType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :description, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
