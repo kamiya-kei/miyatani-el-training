86a99c764ebf3b363dd401d2aa41d8ee4a33ae61
kamiya-kei <miyahei38@gmail.com>
2021-11-08 15:17:57 +0900

    reactコントローラー作成・ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'tests#index'
-
   post "/graphql", to: "graphql#execute"
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
+
+  root to: 'react#index'
+  get '*path', to: 'react#index'
 end
