59d10536498ba28b539eedcc19ddb0a698b47f56
kamiya-kei <miyahei38@gmail.com>
2021-11-09 11:23:03 +0900

    タスク作成ボタン追加

--- app/javascript/components/BaseLayout.js
+++ app/javascript/components/BaseLayout.js
@@ -7,6 +7,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import MuiLink from '@mui/material/Link';
+import Button from '@mui/material/Button';
 
 const BaseLayout = (props) => {
   return (
@@ -14,9 +15,10 @@ const BaseLayout = (props) => {
       <CssBaseline />
       <AppBar>
         <Toolbar>
-          <Typography variant="h6">
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             <MuiLink component={Link} to="/" color="inherit" underline="none">タスク管理システム</MuiLink>
           </Typography>
+          <Button component={Link} to="/tasks/new" color="inherit">タスク作成</Button>
         </Toolbar>
       </AppBar>
       <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
