7e9d58a896722635cb0c0cb6038e164a6ef5893d
kamiya-kei <miyahei38@gmail.com>
2021-11-10 17:52:45 +0900

    exec rubocop -a

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :system do
+RSpec.describe 'Tasks', type: :system do
   describe 'タスク作成' do
     before do
       @task = FactoryBot.build(:task)
@@ -10,17 +10,17 @@ RSpec.describe "Tasks", type: :system do
       visit root_path
       expect(page).to have_content('タスク作成')
       click_link 'タスク作成' # タスク作成ページへ
-      expect(current_path).to eq('/tasks/new')
+      expect(page).to have_current_path('/tasks/new')
 
       # 値を入力して投稿
       fill_in 'title', with: @task.title
       fill_in 'description', with: @task.description
-      expect{
+      expect {
         click_button '投稿'
-      }.to change{Task.count}.by(1)
-      expect(current_path).to eq('/')
+      }.to change { Task.count }.by(1)
+      expect(page).to have_current_path('/')
       expect(page).to have_content('タスクを投稿しました')
-      
+
       # タスク一覧でタスクが投稿されていることを確認
       expect(page).to have_content(@task.title)
       expect(page).to have_content(@task.description)
@@ -30,9 +30,9 @@ RSpec.describe "Tasks", type: :system do
       visit 'tasks/new'
       expect(page).to have_no_content('タイトルを入力してください')
       # 何も入力せず空のまま投稿
-      expect{
+      expect {
         click_button '投稿'
-      }.not_to change{Task.count}
+      }.not_to change { Task.count }
       # バリデーションエラーが表示される
       expect(page).to have_content('タイトルを入力してください')
     end
@@ -42,9 +42,9 @@ RSpec.describe "Tasks", type: :system do
       # 値を入力してキャンセル
       fill_in 'title', with: @task.title
       fill_in 'description', with: @task.description
-      expect{
+      expect {
         click_link 'キャンセル'
-      }.not_to change{Task.count}
+      }.not_to change { Task.count }
 
       # タスク一覧でタスクが投稿されていないことを確認
       expect(page).to have_no_content(@task.title)
@@ -61,7 +61,7 @@ RSpec.describe "Tasks", type: :system do
       it 'タスク編集実行' do
         visit root_path
         click_link '編集' # 編集ページへ
-        expect(current_path).to eq("/tasks/#{@task.id}/edit")
+        expect(page).to have_current_path("/tasks/#{@task.id}/edit", ignore_query: true)
 
         # 変更前の値が入力欄にセットされている
         expect(find_field('title').value).to eq(@task.title)
@@ -71,10 +71,10 @@ RSpec.describe "Tasks", type: :system do
         @task2 = FactoryBot.build(:task)
         fill_in 'title', with: @task2.title
         fill_in 'description', with: @task2.description
-        expect{
+        expect {
           click_button '更新'
-        }.not_to change{Task.count}
-        expect(current_path).to eq('/')
+        }.not_to change { Task.count }
+        expect(page).to have_current_path('/')
         expect(page).to have_content('タスクを更新しました')
 
         # タスク一覧でタスクが更新されていることを確認
@@ -93,10 +93,10 @@ RSpec.describe "Tasks", type: :system do
         @task2 = FactoryBot.build(:task)
         fill_in 'title', with: @task2.title
         fill_in 'description', with: @task2.description
-        expect{
+        expect {
           click_link 'キャンセル'
-        }.not_to change{Task.count}
-        expect(current_path).to eq('/')
+        }.not_to change { Task.count }
+        expect(page).to have_current_path('/')
 
         # タスク一覧でタスクが更新されていないことを確認
         expect(page).to have_content(@task.title)
@@ -112,11 +112,11 @@ RSpec.describe "Tasks", type: :system do
         click_button '削除'
         expect(page).to have_content('本当によろしいですか？')
         sleep 1 # 確認ダイアログの表示アニメーションを待つ
-        expect{
+        expect {
           click_button 'はい'
           # 下記1行はajax終了後にTask.countをチェックさせる為にexpec{}内に入れてます
           expect(page).to have_content('タスクが削除されました')
-        }.to change{Task.count}.by(-1)
+        }.to change { Task.count }.by(-1)
         expect(page).to have_no_content(@task.title)
         expect(page).to have_no_content(@task.description)
       end
@@ -126,9 +126,9 @@ RSpec.describe "Tasks", type: :system do
         click_button '削除'
         expect(page).to have_content('本当によろしいですか？')
         sleep 1 # 確認ダイアログの表示アニメーションを待つ
-        expect{
+        expect {
           click_button 'いいえ'
-        }.not_to change{Task.count}
+        }.not_to change { Task.count }
         expect(page).to have_content(@task.title)
         expect(page).to have_content(@task.description)
       end
