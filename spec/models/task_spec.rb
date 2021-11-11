4bcc1c8761c2ca7df899b8c31dbac5a07f998287
kamiya-kei <miyahei38@gmail.com>
2021-11-11 11:06:34 +0900

    Rubocop手動修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,27 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe '#create' do
-    before do
-      @task = FactoryBot.build(:task)
+    let(:task) { FactoryBot.build(:task, params) }
+
+    context '全ての値がセットされているとき' do
+      let(:params) { {} }
+      it { expect(task).to be_valid }
     end
 
-    context 'タスクが作成できるとき' do
-      it '全ての値がセットされていると作成できる' do
-        expect(@task).to be_valid
-      end
-
-      it 'タスクの説明が空でも作成できる' do
-        @task.description = ''
-        expect(@task).to be_valid
-      end
+    context 'タスクの説明が空のとき' do
+      let(:params) { { description: '' } }
+      it { expect(task).to be_valid }
     end
 
-    context 'タスクが作成できないとき' do
-      it 'タスクのタイトルが空だと作成できない' do
-        @task.title = ''
-        @task.valid?
-        expect(@task.errors.full_messages).to include("Title can't be blank")
-      end
+    context 'タスクのタイトルが空のとき' do
+      let(:params) { { title: '' } }
+      it {
+        task.valid?
+        expect(task.errors.full_messages).to include("Title can't be blank")
+      }
     end
   end
 end
