267651f3546fed9e810759e33387ea74471e8f83
kamiya-kei <miyahei38@gmail.com>
2021-11-12 15:13:16 +0900

    タスクにステータス追加(migration, ActiveHash)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :done
+
   validates :title, presence: true
+  validates :done_id, inclusion: { in: Done.pluck(:id) }
 end
