de10ed55a3717892aaffb3c2030fdf79d079cb1b
kamiya-kei <miyahei38@gmail.com>
2021-11-16 11:34:45 +0900

    検索処理をモデルに移行

--- app/graphql/resolvers/tasks.rb
+++ app/graphql/resolvers/tasks.rb
@@ -9,21 +9,11 @@ module Resolvers
     argument :target,    String,  required: false
 
     def resolve(**args)
-      return Task.all.order('created_at': 'DESC') if args.empty?
-
-      sort_key = args[:sort_type].underscore # スネークケース
-      sort_val = args[:is_asc] ? 'ASC' : 'DESC'
-      case args[:target]
-      when 'all' then
-        tasks = Task.where('title LIKE ? OR description LIKE ?', "%#{args[:word]}%", "%#{args[:word]}%")
-      when 'title' then
-        tasks = Task.where('title LIKE ?', "%#{args[:word]}%")
-      when 'description' then
-        tasks = Task.where('description LIKE ?', "%#{args[:word]}%")
+      if args.empty?
+        Task.all.order('created_at': 'DESC') 
+      else
+        Task.search(**args.slice(:word, :target, :done_ids, :sort_type, :is_asc))
       end
-      tasks
-        .where(done_id: args[:done_ids])
-        .order(sort_key => sort_val)
     end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,23 @@ class Task < ApplicationRecord
 
   validates :title, presence: true
   validates :done_id, inclusion: { in: Done.pluck(:id) }
+
+  def self.search(word:, target:, done_ids:, sort_type:, is_asc:)
+    sort_key = sort_type.underscore # スネークケースに変換
+    sort_val = is_asc ? 'ASC' : 'DESC'
+    Task.where(done_id: done_ids)
+        .search_word("%#{word}%", target)
+        .order(sort_key => sort_val)
+  end
+
+  scope :search_word, ->(word_ptn, target) do
+    case target
+    when 'all'
+        where('title LIKE ? OR description LIKE ?', word_ptn, word_ptn)
+    when 'title'
+        where('title LIKE ?', word_ptn)
+    when 'description'
+        where('description LIKE ?', word_ptn)
+    end
+  end
 end
