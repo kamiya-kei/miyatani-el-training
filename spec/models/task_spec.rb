52c8e793c517ec096af83230e9f3d91673a2c16c
kamiya-kei <miyahei38@gmail.com>
2021-11-16 12:06:42 +0900

    タスクのmodel specにステータスのテストを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,27 +4,40 @@ RSpec.describe Task, type: :model do
   describe '#validate' do
     let(:task) { FactoryBot.build(:task, params) }
 
-    context '全ての値がセットされているとき' do
-      let(:params) { {} }
-      it { expect(task).to be_valid }
+    describe '正常系' do
+      subject { task }
+
+      context '全ての値がセットされているとき' do
+        let(:params) { {} }
+        it { is_expected.to be_valid }
+      end
+
+      context 'タスクの説明が空のとき' do
+        let(:params) { { description: '' } }
+        it { is_expected.to be_valid }
+      end
+
+      context 'タスクの期限が空のとき' do
+        let(:params) { { deadline: '' } }
+        it { is_expected.to be_valid }
+      end
     end
 
-    context 'タスクの説明が空のとき' do
-      let(:params) { { description: '' } }
-      it { expect(task).to be_valid }
-    end
-
-    context 'タスクのタイトルが空のとき' do
-      let(:params) { { title: '' } }
-      it {
+    describe '異常系' do
+      subject {
         task.valid?
-        expect(task.errors.full_messages).to include("Title can't be blank")
+        task.errors.full_messages
       }
-    end
 
-    context 'タスクの期限が空のとき' do
-      let(:params) { { deadline: '' } }
-      it { expect(task).to be_valid }
+      context 'タスクのタイトルが空のとき' do
+        let(:params) { { title: '' } }
+        it { is_expected.to include("Title can't be blank") }
+      end
+
+      context 'タスクのステータスが-1,0,1以外の時' do
+        let(:params) { { done_id: 2 } }
+        it { is_expected.to include('Done is not included in the list') }
+      end
     end
   end
 end
