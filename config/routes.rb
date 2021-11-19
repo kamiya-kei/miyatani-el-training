bba0b82ef98e2bfdf621dd3a8a093d15a58c70bb
kamiya-kei <miyahei38@gmail.com>
2021-11-19 14:08:34 +0900

    userコントローラー・ルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,6 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
 
-  scope :users, format: 'json' do
-    # post 'sign_up',  to: 'users#sign_up'
-    post 'sign_in',  to: 'users#sign_in'
-    post 'signed_in', to: 'users#signed_in'
-    post 'sign_out', to: 'users#sign_out'
-  end
-
   root to: 'react#index'
   get '*path', to: 'react#index'
 end
