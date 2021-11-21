cfb74d740e7630b56c35e34670bd9bdd747ef7c3
kamiya-kei <miyahei38@gmail.com>
2021-11-21 20:37:47 +0900

    MyRouter分離

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,29 +1,11 @@
-import React, { useContext } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import UserProvider from 'common/UserProvider';
-import { UserContext } from 'utils/contexts';
-
-import Top from 'page/Top';
-import TaskPost from 'page/TaskPost';
-import TaskEdit from 'page/TaskEdit';
-import SignIn from 'page/SignIn';
-import SignUp from 'page/SignUp';
-import UnSignedTop from 'page/UnSignedTop';
+import MyRouter from 'MyRouter';
 
 const App = () => {
-  const { isLogin } = useContext(UserContext);
-
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={isLogin ? <Top /> : <UnSignedTop />} />
-          <Route path="/tasks/new" element={<TaskPost />} />
-          <Route path="/tasks/:id/edit" element={<TaskEdit />} />
-          <Route path="/users/sign_in" element={<SignIn />} />
-          <Route path="/users/sign_up" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <MyRouter />
     </UserProvider>
   );
 };
---
+++ app/javascript/components/MyRouter.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserContext } from 'utils/contexts';
+
+import Top from 'page/Top';
+import TaskPost from 'page/TaskPost';
+import TaskEdit from 'page/TaskEdit';
+import SignIn from 'page/SignIn';
+import SignUp from 'page/SignUp';
+import UnSignedTop from 'page/UnSignedTop';
+
+const MyRouter = () => {
+  const { isLogin } = useContext(UserContext);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isLogin ? <Top /> : <UnSignedTop />} />
+        <Route path="/tasks/new" element={<TaskPost />} />
+        <Route path="/tasks/:id/edit" element={<TaskEdit />} />
+        <Route path="/users/sign_in" element={<SignIn />} />
+        <Route path="/users/sign_up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default MyRouter;
