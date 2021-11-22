8ce3721e89734d8c705858eddfd751d98bfb116c
kamiya-kei <miyahei38@gmail.com>
2021-11-22 18:02:22 +0900

    共通機能(確認ダイアログ・フラッシュメッセージ・ローディング)分離

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import UserProvider from 'common/UserProvider';
+import UtilProvider from 'common/UtilProvider';
 import MyApolloProvider from 'common/MyApolloProvider';
 import MyRouter from 'MyRouter';
 
 const App = () => {
   return (
     <MyApolloProvider>
-      <UserProvider>
-        <MyRouter />
-      </UserProvider>
+      <UtilProvider>
+        <UserProvider>
+          <MyRouter />
+        </UserProvider>
+      </UtilProvider>
     </MyApolloProvider>
   );
 };
--- app/javascript/components/utils/contexts.tsx
+++ app/javascript/components/utils/contexts.tsx
@@ -1,5 +1,4 @@
 import { createContext } from 'react';
-
 interface User {
   id: string;
   name: string;
@@ -9,10 +8,24 @@ interface UserContext {
   user: User | null;
   setUser: (user: User) => void;
   isLogin: boolean;
+  loadingUser: boolean;
 }
 
 export const UserContext = createContext({
   user: null,
   setUser: () => null,
   isLogin: false,
+  loadingUser: true,
 } as UserContext);
+
+interface UtilContext {
+  util: {
+    flashMessage: (message) => void;
+    confirmDialog: () => Promise<boolean>;
+    setBackdrop: (loading: boolean) => void;
+  };
+}
+
+export const UtilContext = createContext({
+  util: null,
+} as UtilContext);
