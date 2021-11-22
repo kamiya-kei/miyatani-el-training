66d6db51f1891f1044a55b583c6996144fc22e14
kamiya-kei <miyahei38@gmail.com>
2021-11-22 18:06:13 +0900

    管理者ページはログインしてないとログインページへ飛ばす

---
+++ app/javascript/components/AdminLayout.tsx
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect } from 'react';
+import { UserContext } from 'utils/contexts';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import MuiLink from '@mui/material/Link';
+import HeaderMenu from 'common/HeaderMenu';
+
+const AdminLayout = (props: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+
+  const { isLogin, loadingUser } = useContext(UserContext);
+
+  useEffect(() => {
+    if (loadingUser) return;
+    if (!isLogin) return navigate('/users/sign_in'); // ログインしてない場合はログインページへ飛ばす // TODO: 管理者権限の確認
+  }, [isLogin, loadingUser]);
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            <MuiLink component={Link} to="/" color="inherit" underline="none">
+              タスク管理システム 管理画面
+            </MuiLink>
+          </Typography>
+          <HeaderMenu />
+        </Toolbar>
+      </AppBar>
+      <Container
+        disableGutters
+        maxWidth="sm"
+        component="main"
+        sx={{ pt: 8, pb: 6 }}
+      >
+        {props.children}
+      </Container>
+    </>
+  );
+};
+
+export default AdminLayout;
