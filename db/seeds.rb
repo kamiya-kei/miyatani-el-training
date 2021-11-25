672673d41214e7e2094459526e34d572810d23f3
kamiya-kei <miyahei38@gmail.com>
2021-11-25 11:00:18 +0900

    管理者アカウントを作成するseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
-user = User.create(name: 'test', password: 'abc123', password_confirmation: 'abc123')
-Task.where(user_id: nil).update(user_id: user.id)
+unless User.exists?(name: 'test')
+  user = User.create(name: 'test', password: 'abc123', password_confirmation: 'abc123')
+  Task.where(user_id: nil).update(user_id: user.id)
+end
+
+unless User.exists?(name: 'admin')
+  User.create(name: 'admin', password: 'abc123', password_confirmation: 'abc123', role_id: Role::ADMIN)
+end
