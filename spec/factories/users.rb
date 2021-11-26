0f788a80b9fa356b4a5c3097e34343899312dd22
kamiya-kei <miyahei38@gmail.com>
2021-11-26 17:24:21 +0900

    userのfactorybot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.last_name }
+    name { Faker::Name.last_name + Faker::Name.first_name }
     password { 'abc123' }
     password_confirmation { password }
     role_id { Role::REGULAR }
