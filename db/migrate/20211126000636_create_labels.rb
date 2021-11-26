333599353cfac6dbc62e60e15414e9758bf0c206
kamiya-kei <miyahei38@gmail.com>
2021-11-26 09:39:50 +0900

    ラベルモデルと中間テーブル作成

---
+++ db/migrate/20211126000636_create_labels.rb
@@ -0,0 +1,10 @@
+class CreateLabels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :labels do |t|
+      t.string :name, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+    add_index :labels, [:user_id, :name], unique: true
+  end
+end
---
+++ spec/factories/task_labels.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :task_label do
+    
+  end
+end
